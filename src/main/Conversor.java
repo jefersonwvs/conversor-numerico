eab7449a91c70eb2f6e8c2aba7ca7ccc8142a23e
Jeferson Willian Vieira Silva <57540297+jefersonwvs@users.noreply.github.com>
2020-08-01 11:24:57 -0400

    pequenas modificações na interface gráfica e no restante do código.

--- src/main/Conversor.java
+++ src/main/Conversor.java
@@ -5,16 +5,16 @@ import java.util.List;
 import java.util.Locale;
 
 public class Conversor {
-    
+
     private static int obtemAlgarismo(char ch) {
-        ch = Character.toUpperCase(ch);        
+        ch = Character.toUpperCase(ch);
         switch (ch) {
             case 'A':
                 return 10;
             case 'B':
                 return 11;
             case 'C':
-                return 12; 
+                return 12;
             case 'D':
                 return 13;
             case 'E':
@@ -25,8 +25,8 @@ public class Conversor {
                 return Integer.parseInt(String.valueOf(ch));
         }
     }
-    
-    private static char configuraAlgarismo(int dig) {        
+
+    private static char configuraAlgarismo(int dig) {
         switch (dig) {
             case 10:
                 return 'A';
@@ -44,67 +44,77 @@ public class Conversor {
                 return String.valueOf(dig).charAt(0);
         }
     }
-    
-    public static String transforma(String numero, int baseOrigem, int baseAlvo) 
-    {        
-        String[] num = numero.split("[.]");
+
+    public static String baseOrigemParaBaseDec(String numero, int baseOrigem, int baseAlvo) {
         
-        int tam = num[0].length() - 1; // num[0]: parte inteira
+        String[] num = numero.split("[.]"); // separação das partes inteiras e fracionárias do número
+
+        int tam = num[0].length() - 1; // num[0]: parte inteira - sempre haverá, ainda que seja 0.
         long inteira = 0;
         for (int i = 0; i <= tam; i++) {
             int x = obtemAlgarismo(num[0].charAt(tam - i));
-            if (x >= baseOrigem)
+            if (x >= baseOrigem) {
                 throw new IllegalArgumentException(String.format("%s é um valor inválido na base %d", numero, baseOrigem));
+            }
             inteira += x * Math.pow(baseOrigem, i);
         }
-        
+
+        if (num.length == 1) { // número é inteiro, isto é, não possui parte decimal
+            return baseDecParaBaseAlvo(String.format("%d", inteira), baseAlvo);
+        }
+
         tam = num[1].length() - 1; // num[1]: parte fracionária
         double fracionario = 0.0;
         for (int i = 0; i <= tam; i++) {
             int x = obtemAlgarismo(num[1].charAt(i));
-            if (x >= baseOrigem)
+            if (x >= baseOrigem) {
                 throw new IllegalArgumentException(String.format("%s é um valor inválido na base %d", numero, baseOrigem));
-            fracionario += x * Math.pow(baseOrigem, -(i+1));
+            }
+            fracionario += x * Math.pow(baseOrigem, -(i + 1));
         }
         Locale.setDefault(Locale.US);
         return baseDecParaBaseAlvo(String.format("%f", inteira + fracionario), baseAlvo);
     }
-    
+
     private static String baseDecParaBaseAlvo(String numero, int baseAlvo) {
+        
         double valor = Double.parseDouble(numero);
-        System.out.println(valor);
-        
-        List<Character> list = new ArrayList<>();
-        
-        long inteira = (int)valor;
-        System.out.println(inteira);
-        
+
+        List<Character> list = new ArrayList<>(); // lista para armazenar os algarismo correspondentes a cada base
+
+        long inteira = (int) valor; // parte inteira do número
+
+        /* Transformação da parte inteira */
         while (inteira / baseAlvo != 0) {
-            int dig = (int)inteira % baseAlvo;
+            int dig = (int) inteira % baseAlvo;
             list.add(0, configuraAlgarismo(dig));
             inteira /= baseAlvo;
         }
-        list.add(0, configuraAlgarismo((int)inteira));
+        list.add(0, configuraAlgarismo((int) inteira));
+
+        /* Verificando se há parte fracionária */
+        double fracionaria = valor - (int) valor;
+        if (fracionaria == 0) { // não há parte fracionário, o número é inteiro
+            return formata(list);
+        }
         
+        /* Transformação da parte decimal*/
         list.add(list.size(), '.');
-        
-        double fracionaria = valor - (int)valor;
-        System.out.println(fracionaria);
         int i = 0;
         while ((fracionaria != 0) && (i < 23)) {
-            int dig = (int)(fracionaria * baseAlvo);
-            System.out.println(dig);
+            int dig = (int) (fracionaria * baseAlvo);
             list.add(list.size(), configuraAlgarismo(dig));
             fracionaria = (fracionaria * baseAlvo) - dig;
             i++;
-        }        
+        }
         return formata(list);
     }
 
     private static String formata(List<Character> list) {
-        System.out.println("cheguei");
         StringBuilder builder = new StringBuilder();
-        list.forEach((a) -> { builder.append(a); });
-        return builder.toString();
+        list.forEach((a) -> {
+            builder.append(a);
+        });
+        return builder.toString().replace(".", ",");
     }
 }
--- src/main/TelaController.java
+++ src/main/TelaController.java
@@ -4,6 +4,7 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
@@ -12,72 +13,62 @@ import javafx.scene.control.ButtonType;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextField;
 
-public class TelaController implements Initializable {
+public class TelaController implements Initializable, EventHandler<javafx.event.ActionEvent> {
 
     //@FXML
     //private TextField txtBaseA;
-    
     @FXML
     private ChoiceBox chBoxBaseA;
-    
+
     @FXML
     private TextField txtNumeroBaseA;
-    
+
     @FXML
     private Button btAparaB;
-    
+
     @FXML
     private ChoiceBox chBoxBaseB;
-    
+
     @FXML
     private TextField txtNumeroBaseB;
-    
+
     @FXML
     private Button btBparaA;
-    
-    @FXML
-    private void acaoBtAparaB() {
-        try {
-            int baseA = Integer.parseInt(chBoxBaseA.getValue().toString());
-            String numero = txtNumeroBaseA.getText();
-            numero = numero.replace(",", ".");
-            int baseB = Integer.parseInt(chBoxBaseB.getValue().toString());
-            txtNumeroBaseB.setText(Conversor.transforma(numero, baseA, baseB));
-        } catch (IllegalArgumentException e) {
-            Alert alerta = new Alert(null);
-            alerta.setTitle("Erro");
-            alerta.setContentText(e.getMessage());
-            alerta.getButtonTypes().add(ButtonType.OK);
-            alerta.show();
-        }
-    }
-    
-    @FXML
-    private void acaoBtBparaA() {
-        try {
-            int baseB = Integer.parseInt(chBoxBaseB.getValue().toString());
-            String numero = txtNumeroBaseB.getText();
-            numero = numero.replace(",", ".");
-            int baseA = Integer.parseInt(chBoxBaseA.getValue().toString());
-            txtNumeroBaseA.setText(Conversor.transforma(numero, baseB, baseA));
-        } catch (IllegalArgumentException e) {
-            Alert alerta = new Alert(null);
-            alerta.setTitle("Erro");
-            alerta.setContentText(e.getMessage());
-            alerta.getButtonTypes().add(ButtonType.OK);
-            alerta.show();
-        }
-        
-    }
-    
-    
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        ObservableList<Integer> bases = FXCollections.observableArrayList(2,3,4,5,6,7,8,9,10,11,12,13,14,15,16);
+        ObservableList<Integer> bases = FXCollections.observableArrayList(2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16);
         chBoxBaseA.getItems().addAll(bases);
         chBoxBaseA.getSelectionModel().select(8);
         chBoxBaseB.getItems().addAll(bases);
         chBoxBaseB.getSelectionModel().select(0);
-    }    
-    
+        btAparaB.setOnAction(this);
+        btBparaA.setOnAction(this);
+    }
+
+    @Override
+    public void handle(javafx.event.ActionEvent event) {
+        try {
+            if (event.getSource() == btAparaB) {
+                int baseA = Integer.parseInt(chBoxBaseA.getValue().toString());
+                String numero = txtNumeroBaseA.getText();
+                numero = numero.replace(",", ".");
+                int baseB = Integer.parseInt(chBoxBaseB.getValue().toString());
+                txtNumeroBaseB.setText(Conversor.baseOrigemParaBaseDec(numero, baseA, baseB));
+            } else {
+                int baseB = Integer.parseInt(chBoxBaseB.getValue().toString());
+                String numero = txtNumeroBaseB.getText();
+                numero = numero.replace(",", ".");
+                int baseA = Integer.parseInt(chBoxBaseA.getValue().toString());
+                txtNumeroBaseA.setText(Conversor.baseOrigemParaBaseDec(numero, baseB, baseA));
+            }
+        } catch (IllegalArgumentException e) {
+            Alert alerta = new Alert(null);
+            alerta.setTitle("Erro");
+            alerta.setContentText(e.getMessage());
+            alerta.getButtonTypes().add(ButtonType.OK);
+            alerta.show();
+        }
+    }
+
 }
